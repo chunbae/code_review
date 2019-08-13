615b1a676fcc8de4653efcdca771e942afaf68b9
chunbae <pchunb@gmail.com>
2019-08-13 16:15:46 +0900

    Reformat codes
    1. Add java doc
    2. reformat codes

--- priority_queue.cc
+++ priority_queue.cc
@@ -1,34 +1,57 @@
 #include "priority_queue.h"
 
 template <>
-templatePriorityQueue<int>::templatePriorityQueue(){
+templatePriorityQueue<int>::templatePriorityQueue() {
 }
 
 template <>
-templatePriorityQueue<int>::~templatePriorityQueue(){
+templatePriorityQueue<int>::~templatePriorityQueue() {
 }
 
+/**
+ * Checks whether the underlying container is empty
+ *
+ * @return true if it's empty otherwise false
+ */
 template <>
-bool templatePriorityQueue<int>::empty() const{
-        return storage.empty();
+bool templatePriorityQueue<int>::empty() const {
+  return storage.empty();
 }
 
+/**
+ * Returns the number of elements
+ *
+ * @return the number of elements
+ */
 template <>
 int templatePriorityQueue<int>::size() {
-        return storage.size();
+  return storage.size();
 }
 
+/**
+ * Accesses the top element
+ *
+ * @return the top element
+ */
 template <>
-const int& templatePriorityQueue<int>::top() const{
-        return storage.top();
+const int& templatePriorityQueue<int>::top() const {
+  return storage.top();
 }
 
+/**
+ * Removes the top element
+ */
 template <>
-void templatePriorityQueue<int>::pop(){
-        storage.pop();
+void templatePriorityQueue<int>::pop() {
+  storage.pop();
 }
 
+/**
+ * Inserts element and sorts the underlying container
+ *
+ * @param val element to push
+ */
 template <>
-void templatePriorityQueue<int>::push(const int& val){
-        storage.push(val);
+void templatePriorityQueue<int>::push(const int& val) {
+  storage.push(val);
 }
